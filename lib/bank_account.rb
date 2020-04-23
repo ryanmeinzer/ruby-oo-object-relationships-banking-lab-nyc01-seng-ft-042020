30a289e38dbeb42b21c4cc2ced047ca1966b0e26
carolineglass <carolineglass89@gmail.com>
2020-04-23 15:02:31 -0400

    first 10 passed

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,24 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
 
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+    def deposit(amount)
+        @balance = @balance + amount
+    end
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+    def valid?
+        (@status == "open") && (@balance > 0)
+    end
+    def close_account
+        @status = "closed"
+    end
 end
 
 #third edit test 2
